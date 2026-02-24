925112a074bfbebfa628c7b8e05681a1789cc217
odiene <odiene@jasmineconseil.com>
2026-02-24 12:27:39 +0000

    add cours

--- lib/modules/admin/widgets/admin_sidebar.dart
+++ lib/modules/admin/widgets/admin_sidebar.dart
@@ -89,7 +89,7 @@ class AdminSidebar extends StatelessWidget {
                   '/admin/silsila',
                 ),
                 _buildMenuItem(
-                  " Gestion Cours",
+                  "Cours",
                   Icons.calendar_month_rounded,
                   '/admin/calendar',
                 ),
